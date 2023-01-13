071df0916bb651dd273db9cccf84cc5dce79b754
Karen Valier <karenvalierpalmero@gmail.com>
2023-01-12 20:17:22 -0600

    feat: se hace la estructura del proyecto

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log('Hola mundo ^^!')
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+app.listen(process.env.PORT, ()=>{
+    console.log('\x1b[34m ******************************************* \x1b[0m');
+    console.log('Se levanto la API')
+    console.log('\x1b[34m ******************************************* \x1b[0m');
+})
+
+//localhost:5000
+app.get ('/',(req, res)=>{
+
+    res.send('Hola mundo');
+
+})
+
+//Collections
+//Users - Customers
